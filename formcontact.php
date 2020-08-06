f32d57e916f4c7333105d38b718b2417d2bec335
TechNCode-Github <68112752+TechNCode-Github@users.noreply.github.com>
2020-08-06 20:16:12 +0200

    Website V1.0

---
+++ formcontact.php
@@ -0,0 +1,10 @@
+<?php
+$headers = 'From: techncode.ds@gmail.com' . "\r\n" .
+'Reply-To: techncode.ds@gmail.com' . "\r\n" .
+'X-Mailer: PHP/' . phpversion();
+$destinataire = 'techncode.ds@gmail.com';
+$user_mail = $_POST["user_mail"];
+$subject = $_POST["sujet"];
+$message = $_POST["message"];
+mail($destinataire, $subject, $message, $user_mail, $header);
+?>
