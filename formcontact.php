c1c375d6c4b526bf9c5d55115cef22210865474d
TechNCode-Github <68112752+TechNCode-Github@users.noreply.github.com>
2020-08-20 16:47:40 +0200

    Add files via upload

--- formcontact.php
+++ formcontact.php
@@ -3,8 +3,9 @@ $headers = 'From: techncode.ds@gmail.com' . "\r\n" .
 'Reply-To: techncode.ds@gmail.com' . "\r\n" .
 'X-Mailer: PHP/' . phpversion();
 $destinataire = 'techncode.ds@gmail.com';
+$user_name = $_POST["user_name"];
 $user_mail = $_POST["user_mail"];
 $subject = $_POST["sujet"];
 $message = $_POST["message"];
-mail($destinataire, $subject, $message, $user_mail, $header);
+mail($destinataire, $subject, $message, $user_name, $user_mail, $headers);
 ?>
